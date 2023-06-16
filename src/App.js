d947a98d364cb5ad85adaa10fb93cb906058e5f4
Aliaksandra Novikava <alexa_nov90@yahoo.de>
2023-06-16 12:39:12 +0200

    add functionality to the form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 import Header from "./components/Header/index.js";
 import Form from ".components/Form/index.js";
 
 function App() {
+  const [activities, setActivities] = useLocalStorageState("actvities", {
+    defaultValue: [],
+  });
+
+  function handleAddActivity(newActivity) {
+    setActivities({ id: uid(), ...newActivity }, ...activities);
+  }
+
+  // setEntries([
+  //   { id: uid(), date, isFavorite: false, ...newEntry },
+  //   ...entries,
+  // ]);
+
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <main className="app__main">
-        <Form />
+        <Form onAddActivity={handleAddActivity} />
       </main>
     </div>
   );
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,21 @@
 import "./Form.css";
 import Button from "../components/Button/index.js";
-
+console.log("test");
 export default function Form(onAddActivity) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    if (data.isForGoodWeather) {
+      data.isForGoodWeather = true;
+    } else {
+      data.isForGoodWeather = false;
+    }
+    onAddActivity(data);
+    event.target.reset();
+    event.target.elements.activity__input.focus();
+  }
+
   return (
     <form className="entry-form">
       <h2 className="entry-form__title">Add new Activity</h2>
@@ -11,6 +25,7 @@ export default function Form(onAddActivity) {
           type="text"
           className="entry-form__field "
           id="activity__input"
+          name="activity__input"
         ></input>
         <label for="checkbox">Good-weather activity:</label>
         <input type="checkbox" id="checkbox"></input>
